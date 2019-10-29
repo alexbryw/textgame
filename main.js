d14c184a299d926d4b0dc87b948574bd26df683d
alexbryw <alexbryw@gmail.com>
2019-10-29 21:01:03 +0100

    added goToRoom function.

--- main.js
+++ main.js
@@ -1,16 +1,17 @@
-let currentRoom = 1;
+let currentRoom = 0;
+
+
+function startGame(){
+    currentRoom = 0;
+    document.querySelector('.text-output').innerHTML = '';
+    addTextToOutput(rooms[currentRoom].roomText);
+}
 
 function getTextButton(){
-    console.log('test');
     let intext = document.getElementById('inTextId').value;
     console.log(intext);
     addTextToOutput(intext);
-    //document.querySelector('.text-output').innerHTML += '<h3>'+intext+'</h3>';
-    /*
-    let oldtext = document.querySelector('.text-output').innerHTML;
-    let newText = '<h3>'+intext+'</h3>' + oldtext;
-    document.querySelector('.text-output').innerHTML = newText;
-    */
+    goToRoom(intext)
 }
 
 function addTextToOutput(intext){
@@ -19,15 +20,43 @@ function addTextToOutput(intext){
     document.querySelector('.text-output').innerHTML = newText;
 }
 
+function goToRoom(roomNumberInput){
+    let roomNotFound = true;
+    for(let possibleRoomNumber in rooms[currentRoom].possibleRooms){
+        if(roomNumberInput === possibleRoomNumber){
+            addTextToOutput('room found!');
+            addTextToOutput(rooms[roomNumberInput-1].roomText); //TODO fix negative number.
+            currentRoom = roomNumberInput-1;
+            roomNotFound = false;
+            break;
+        }
+    }
+    if(roomNotFound === true){
+        addTextToOutput('That is not possible, try agin.');
+    }
+    
+    
+}
+
 const rooms = [
-    room1= {
+    {
+        roomNumber: 0,
         roomText: 'room 1 text, press 1 for red door or 2 for green door',
-        possibleRooms: [1,2]
+        possibleRooms: [2,3]
     },
-    room2= {
-        roomText: 'room 2 text, press 1 for teel door or 2 for purple door',
-        possibleRooms: [3,4]
+    {
+        roomNumber: 1,
+        roomText: 'room 2 text,\npress 1 for teel door or 2 for purple door',
+        possibleRooms: [1,3]
+    },
+    {
+        roomNumber: 2,
+        roomText: 'room 3 text,\npress1 press 2',
+        possibleRooms: [1,2]
     }
 ]
 
-console.log(rooms[1].roomText);
+startGame();
+
+//console.log(rooms[1].roomText);
+//addTextToOutput(rooms[currentRoom].roomText)
