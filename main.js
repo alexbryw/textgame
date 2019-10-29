135f5c2f103fa9665c14a6ebbd8d00b1a4d90218
alexbryw <alexbryw@gmail.com>
2019-10-29 11:54:17 +0100

    Text output innerHTML

--- main.js
+++ main.js
@@ -2,4 +2,5 @@ function getTextButton(){
     console.log('test');
     let intext = document.getElementById('inTextId').value;
     console.log(intext);
+    document.getElementById('text-output').innerHTML += '<h3>'+intext+'</h3>';
 }
