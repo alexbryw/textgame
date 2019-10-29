facce57d540085798796154139fe8b7ded389965
alexbryw <alexbryw@gmail.com>
2019-10-29 18:32:21 +0100

    Added rooms array of objects.

--- main.js
+++ main.js
@@ -1,15 +1,4 @@
-const room0 = 'Du ser två dörrar välj 1 eller 2.';
-
-let room = [
-    'room1 : ',
-    'room2 : ',
-    'room3 : '
-]
-
-addTextToOutput(room[0]);
-
-addTextToOutput(room0);
-//document.querySelector('.text-output').innerHTML
+let currentRoom = 1;
 
 function getTextButton(){
     console.log('test');
@@ -29,3 +18,16 @@ function addTextToOutput(intext){
     let newText = '<h3>'+intext+'</h3>' + oldtext;
     document.querySelector('.text-output').innerHTML = newText;
 }
+
+const rooms = [
+    room1= {
+        roomText: 'room 1 text, press 1 for red door or 2 for green door',
+        possibleRooms: [1,2]
+    },
+    room2= {
+        roomText: 'room 2 text, press 1 for teel door or 2 for purple door',
+        possibleRooms: [3,4]
+    }
+]
+
+console.log(rooms[1].roomText);
